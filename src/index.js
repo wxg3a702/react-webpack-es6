c98ebd402b041657bb587b9495f13a8b194a7c84
xgwen <xgwen@xgwendeMacBook-Pro.local>
2018-01-11 15:56:40 +0800

    feat: create project.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import './css/index.css';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            message: "Hello World!!!"
+        }
+    }
+
+    onChange = () => {
+        this.setState({
+            message: Math.random(),
+        })
+    };
+
+    render() {
+        return (
+            <div className='container'>
+                <h1 className='h1'>{this.state.message}</h1>
+                <button className='button' onClick={this.onChange}>cilck me</button>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<App/>, document.getElementById('root'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: ['babel-polyfill', path.resolve(__dirname, './src/index.js')],
+    output: {
+        path: path.resolve(__dirname, 'build'), //打包文件的输出路径
+        filename: 'bundle.js' //打包文件名
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack测试",
+            template: './public/index.html', //指定模板路径
+            filename: 'index.html', //指定文件名
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    module: {
+        rules: [ //配置加载器
+            {
+                test: /\.js$/, //配置要处理的文件格式，一般使用正则表达式匹配
+                loader: 'babel-loader', //使用的加载器名称
+                query: { //babel的配置参数，可以写在.babelrc文件里也可以写在这里
+                    presets: ['env', 'react', 'stage-2']
+                }
+            },
+            {
+                test: /\.css/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'build'),
+        hot: true,
+    },
+};
